cc6be1badc165a1f2379f2c711dca26aa15c34d4
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-19 01:35:13 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello REVIEWDOG!");
     }
+    void myMethod(){ 
+    int x= 10; 
+    System.out.println("Checkstyle Test");
+    }
 }
